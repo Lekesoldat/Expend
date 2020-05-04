df9d4e74dd1d19aed590748396cfd7f7f1e50e7c
Magnus Lauritzen Holtet <magnulho@gmail.com>
2020-05-04 21:46:29 +0200

    Add introspection.

--- packages/backend/src/index.js
+++ packages/backend/src/index.js
@@ -6,7 +6,12 @@ const { ApolloServer } = apollo;
 const main = async () => {
   const { resolvers } = await resolve();
 
-  const server = new ApolloServer({ typeDefs, resolvers, playground: true });
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    playground: true,
+    introspection: true,
+  });
 
   server.listen(process.env.PORT).then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
