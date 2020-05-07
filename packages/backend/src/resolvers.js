b0b91976368ea39f746e1e7d1e9208075d5faed5
Magnus Lauritzen Holtet <magnulho@gmail.com>
2020-05-08 01:43:14 +0200

    Create subscription model.

--- packages/backend/src/resolvers.js
+++ packages/backend/src/resolvers.js
@@ -1,38 +1,11 @@
-import Sequelize from "sequelize";
 import connect from "./connectors.js";
 
 const resolve = async () => {
-  const { IncomeCategory, Income, ExpenseCategory, Expense } = await connect();
+  const { Subscription } = await connect();
 
   const resolvers = {
     Query: {
-      expenseCategories: async () =>
-        await ExpenseCategory.findAll({ include: Expense }),
-
-      incomeCategories: async () =>
-        await IncomeCategory.findAll({ include: Income }),
-
-      expenses: async () => await Expense.findAll({ include: ExpenseCategory }),
-
-      incomes: async () => await Income.findAll({ include: IncomeCategory }),
-
-      subscriptions: async () =>
-        await Expense.findAll({
-          where: {
-            isSubscription: {
-              [Sequelize.Op.is]: true,
-            },
-          },
-        }),
-
-      regularIncome: async () =>
-        await Income.findAll({
-          where: {
-            isRegularIncome: {
-              [Sequelize.Op.is]: true,
-            },
-          },
-        }),
+      subscriptions: async () => await Subscription.findAll(),
     },
   };
 
